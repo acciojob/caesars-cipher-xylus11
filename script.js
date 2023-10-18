9b357f7b0f80ece85f484c2fd1fa06513e3a8bad
mangeshahireakaXylus11 <97844580+xylus11@users.noreply.github.com>
2023-10-18 23:38:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -34,14 +34,15 @@ const lookup = {
 function rot13(encodedStr) {
   let decodedArr = []; // Your Result goes here
   // Only change code below this line
+	let Str = encodedStr.toUpperCase();
 	let a="";
-	for(let i =0;i<encodedStr.length;i++)
+	for(let i =0;i<Str.length;i++)
 		{
-			a+=lookup[encodedStr[i];
+			a+=lookup[Str[i];
 			
 		}
 
-  return; //return decodedArr
+  return a; //return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
